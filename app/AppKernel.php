cf9ed301e9710a8fa7471d018dd6cfe2352ea823
Piotr Kedra <piotr@kraksoft.pl>
2015-02-28 12:20:13 +0100

    Change configuration to meet custom vagrant box

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -4,6 +4,8 @@ use Sylius\Bundle\CoreBundle\Kernel\Kernel;
 
 class AppKernel extends Kernel
 {
+    protected $projectName = 'sylius-standard/custom';
+
     public function registerBundles()
     {
         $bundles = array(
@@ -15,5 +17,31 @@ class AppKernel extends Kernel
         }
 
         return array_merge(parent::registerBundles(), $bundles);
+    }
+
+    public function getCacheDir()
+    {
+        if (!empty($_SERVER['SYMFONY_USE_SHM']) && in_array($this->environment, array(
+                'dev',
+                'test'
+            ))
+        ) {
+            return '/dev/shm/' . $this->projectName . '/cache/' . $this->environment;
+        }
+
+        return parent::getCacheDir();
+    }
+
+    public function getLogDir()
+    {
+        if (!empty($_SERVER['SYMFONY_USE_SHM']) && in_array($this->environment, array(
+                'dev',
+                'test'
+            ))
+        ) {
+            return '/dev/shm/' . $this->projectName . '/logs';
+        }
+
+        return parent::getLogDir();
     }
 }
